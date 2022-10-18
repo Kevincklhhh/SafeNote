73c8d435c3130f888cf565b5fe5d190527bacdfa
Kevincklhhh <Kevin_ckl@outlook.com>
2022-10-17 22:49:03 -0400

    tested keystore and cipher

--- app/src/main/java/com/example/safenote/MainActivity.java
+++ app/src/main/java/com/example/safenote/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.safenote;
 
+import static com.example.safenote.SetPasswordActivity.hashPassword;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,11 +12,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.AppBarConfiguration;
 
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.util.Arrays;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
@@ -30,8 +43,40 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor myEdit = sh.edit();
         login.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {// TODO: Check password correspondence
-                Intent intent = new Intent(getApplicationContext(), NoteActivity.class);
-                startActivity(intent);
+                byte[] decodedStoredPH = null;
+                byte[] decryptedstoredPH = null;
+                KeyManager km = new KeyManager();
+                SharedPreferences sh = getSharedPreferences("shared_preference", MODE_PRIVATE);//store password in shared preference
+                String storedPasswordHash = sh.getString("password", "");
+                try {
+                    decodedStoredPH = Base64.decode(storedPasswordHash.getBytes("UTF-8"), Base64.DEFAULT);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    decryptedstoredPH = km.decrypt(getApplicationContext(), decodedStoredPH);
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (NoSuchPaddingException e) {
+                    e.printStackTrace();
+                } catch (NoSuchProviderException e) {
+                    e.printStackTrace();
+                } catch (BadPaddingException e) {
+                    e.printStackTrace();
+                } catch (IllegalBlockSizeException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    if (Arrays.equals(hashPassword(passwordInput.getText().toString()),decryptedstoredPH) || storedPasswordHash.equals("")) {
+                        Intent intent = new Intent(getApplicationContext(), NoteActivity.class);
+                        startActivity(intent);
+                    }
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
         Button setPassword = findViewById(R.id.SetPasswordButton);
